ac4eb95c1007f93abf26b3c9cce2d57ebf51ece7
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-03-29 23:48:24 +0800

    fix font size

--- src/Components/User/Favorites.tsx
+++ src/Components/User/Favorites.tsx
@@ -47,24 +47,24 @@ const Favorites: React.FC = () => {
 
         return (
             <div className='w-full mt-4' >
-                <div className='text-xs flex' >
+                <div className='flex' >
                     <select 
                     onChange={handleSort} 
-                    className='cursor-pointer select-none p-1 mr-3 rounded outline-none border text-gray-400 focus-within:text-black hover:text-black' >
+                    className='text-xs cursor-pointer select-none p-1 mr-3 rounded outline-none border text-gray-400 focus-within:text-black hover:text-black' >
                         <option value="name">Sort by Name</option>
                         <option value="id">Sort by ID</option>
                     </select>
                     <select 
                     onChange={handleFilter} 
-                    className='cursor-pointer select-none p-1 mr-3 rounded outline-none border text-gray-400 focus-within:text-black hover:text-black' >
+                    className='text-xs cursor-pointer select-none p-1 mr-3 rounded outline-none border text-gray-400 focus-within:text-black hover:text-black' >
                         <option value="">Filter by type</option>
                         {typeList && typeList.map( type => <option key={type} value={type}>{type}</option> )}
                     </select>
-                    <div className="relative text-gray-400 focus-within:text-black hover:text-black">
+                    <div className="relative text-xs text-gray-400 focus-within:text-black hover:text-black">
                         <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                             <FaSearch/>
                         </span>
-                        <input id="searchpokemon" type="search" name="q" value={search} className="py-2 text-sm pl-10 outline-none rounded border text-gray-900 w-full" placeholder="Enter Pokemon Name..." autoComplete="off" onChange={handleSearch} />
+                        <input id="searchpokemon" type="search" name="q" value={search} className="text-xs py-2 pl-10 outline-none rounded border text-gray-900 w-full" placeholder="Enter Pokemon Name..." autoComplete="off" onChange={handleSearch} />
                     </div>
                 </div>
                 <div className='flex flex-wrap justify-center mt-2' >
